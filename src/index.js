03d5d56de0303463d568e39354670f586dcb878e
Ronaldo Goncalves <rgoncalves1026@gmail.com>
2021-03-18 20:30:03 +0000

    My first react app

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Hello from "./components/hello";
 
 ReactDOM.render(
   <React.StrictMode>
